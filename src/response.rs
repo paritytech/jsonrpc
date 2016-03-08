f1fc1d75d56b4d0014af8f4d0450900f632c0ae7
Jacob Payne <jacobapayne@gmail.com>
2016-03-07 22:16:50 -0800

    Add Unit Tests for Output and Response Deserialization

--- src/response.rs
+++ src/response.rs
@@ -2,21 +2,21 @@
 use serde::{Serialize, Serializer};
 use super::{Id, Value, Error, Version};
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Success {
 	pub jsonrpc: Version,
 	pub result: Value,
 	pub id: Id
 }
 
-#[derive(Debug, PartialEq, Serialize)]
+#[derive(Debug, PartialEq, Serialize, Deserialize)]
 pub struct Failure {
 	pub jsonrpc: Version,
 	pub error: Error,
 	pub id: Id
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Deserialize)]
 pub enum Output {
 	Success(Success),
 	Failure(Failure)
@@ -32,7 +32,7 @@ impl Serialize for Output {
 	}
 }
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Deserialize)]
 pub enum Response {
 	Single(Output),
 	Batch(Vec<Output>)
@@ -64,6 +64,21 @@ fn success_output_serialize() {
 }
 
 #[test]
+fn success_output_deserialize() {
+	use serde_json;
+	use serde_json::Value;
+
+	let dso = r#"{"jsonrpc":"2.0","result":1,"id":1}"#;
+
+	let deserialized: Output = serde_json::from_str(dso).unwrap();
+	assert_eq!(deserialized, Output::Success(Success {
+		jsonrpc: Version::V2,
+		result: Value::U64(1),
+		id: Id::Num(1)
+	}));
+}
+
+#[test]
 fn failure_output_serialize() {
 	use serde_json;
 	use serde_json::Value;
@@ -75,6 +90,60 @@ fn failure_output_serialize() {
 	});
 
 	let serialized = serde_json::to_string(&fo).unwrap();
-	assert_eq!(serialized, r#"{"jsonrpc":"2.0","error":{"code":-32700,"message":"Parse error"},"id":1}"#);
+	assert_eq!(serialized, r#"{"jsonrpc":"2.0","error":{"code":-32700,"message":"Parse error","data":null},"id":1}"#);
 }
+
+#[test]
+fn failure_output_deserialize() {
+	use serde_json;
+	use serde_json::Value;
+
+	let dfo = r#"{"jsonrpc":"2.0","error":{"code":-32700,"message":"Parse error"},"id":1}"#;
+
+	let deserialized: Output = serde_json::from_str(dfo).unwrap();
+	assert_eq!(deserialized, Output::Failure(Failure {
+		jsonrpc: Version::V2,
+		error: Error::parse_error(),
+		id: Id::Num(1)
+	}));
+}
+
+#[test]
+fn single_response_deserialize() {
+	use serde_json;
+	use serde_json::Value;
+
+	let dsr = r#"{"jsonrpc":"2.0","result":1,"id":1}"#;
+
+	let deserialized: Response = serde_json::from_str(dsr).unwrap();
+	assert_eq!(deserialized, Response::Single(Output::Success(Success {
+		jsonrpc: Version::V2,
+		result: Value::U64(1),
+		id: Id::Num(1)
+	})));
+}
+
+#[test]
+fn batch_response_deserialize() {
+	use serde_json;
+	use serde_json::Value;
+
+	let dbr = r#"[{"jsonrpc":"2.0","result":1,"id":1},{"jsonrpc":"2.0","error":{"code":-32700,"message":"Parse error"},"id":1}]"#;
+
+	let deserialized: Response = serde_json::from_str(dbr).unwrap();
+	assert_eq!(deserialized, Response::Batch(vec![
+		Output::Success(Success {
+			jsonrpc: Version::V2,
+			result: Value::U64(1),
+			id: Id::Num(1)
+		}),
+		Output::Failure(Failure {
+			jsonrpc: Version::V2,
+			error: Error::parse_error(),
+			id: Id::Num(1)
+		})
+	]));
+}
+
+
 
