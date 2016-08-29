529c11a8eab10d47825a1ba3d72fd617e335476d
Tomusdrw <tomusdrw@gmail.com>
2016-08-29 10:41:53 +0200

    Fixing indentation

--- examples/server.rs
+++ examples/server.rs
@@ -7,20 +7,20 @@ use jsonrpc_http_server::*;
 
 struct SayHello;
 impl SyncMethodCommand for SayHello {
-    fn execute(&self, _params: Params) -> Result<Value, Error> {
-        Ok(Value::String("hello".to_string()))
-    }
+	fn execute(&self, _params: Params) -> Result<Value, Error> {
+		Ok(Value::String("hello".to_string()))
+	}
 }
 
 fn main() {
-    let io = IoHandler::new();
-    io.add_method("say_hello", SayHello);
+	let io = IoHandler::new();
+	io.add_method("say_hello", SayHello);
 
-    let server = ServerBuilder::new(Arc::new(io))
-			.cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
-			.start_http(&"127.0.0.1:3030".parse().unwrap())
-			.expect("Unable to start RPC server");
+	let server = ServerBuilder::new(Arc::new(io))
+		.cors(DomainsValidation::AllowOnly(vec![AccessControlAllowOrigin::Null]))
+		.start_http(&"127.0.0.1:3030".parse().unwrap())
+		.expect("Unable to start RPC server");
 
-		server.wait().unwrap();
+	server.wait().unwrap();
 }
 
