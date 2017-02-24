56db68931de25c9a3c35136f3bb621a35de5bd8d
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2017-02-24 13:56:22 +0100

    Re-formatting tcp (#47)

--- tcp/src/server.rs
+++ tcp/src/server.rs
@@ -1,16 +1,3 @@
-// Copyright 2015, 2016 Ethcore (UK) Ltd.
-// This file is part of Parity.
-
-// Parity is free software: you can redistribute it and/or modify
-// it under the terms of the GNU General Public License as published by
-// the Free Software Foundation, either version 3 of the License, or
-// (at your option) any later version.
-
-// Parity is distributed in the hope that it will be useful,
-// but WITHOUT ANY WARRANTY; without even the implied warranty of
-// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-// GNU General Public License for more details.
-
 use std;
 use std::net::SocketAddr;
 use std::sync::Arc;
@@ -29,94 +16,92 @@ use meta::{MetaExtractor, RequestContext, NoopExtractor};
 use dispatch::{Dispatcher, SenderChannels, PeerMessageQueue};
 
 pub struct Server<M: Metadata = (), S: Middleware<M> = NoopMiddleware> {
-    listen_addr: SocketAddr,
-    handler: Arc<MetaIoHandler<M, S>>,
-    meta_extractor: Arc<MetaExtractor<M>>,
-    channels: Arc<SenderChannels>,
+	listen_addr: SocketAddr,
+	handler: Arc<MetaIoHandler<M, S>>,
+	meta_extractor: Arc<MetaExtractor<M>>,
+	channels: Arc<SenderChannels>,
 }
 
 impl<M: Metadata, S: Middleware<M> + 'static> Server<M, S> {
-    pub fn new(addr: SocketAddr, handler: Arc<MetaIoHandler<M, S>>) -> Self {
-        Server {
-            listen_addr: addr,
-            handler: handler,
-            meta_extractor: Arc::new(NoopExtractor),
-            channels: Default::default(),
-        }
-    }
+	pub fn new(addr: SocketAddr, handler: Arc<MetaIoHandler<M, S>>) -> Self {
+		Server {
+			listen_addr: addr,
+			handler: handler,
+			meta_extractor: Arc::new(NoopExtractor),
+			channels: Default::default(),
+		}
+	}
 
-    pub fn extractor(mut self, meta_extractor: Arc<MetaExtractor<M>>) -> Self {
-        self.meta_extractor = meta_extractor;
-        self
-    }
+	pub fn extractor(mut self, meta_extractor: Arc<MetaExtractor<M>>) -> Self {
+		self.meta_extractor = meta_extractor;
+		self
+	}
 
-    pub fn run(&self) -> std::io::Result<()> {
-        let mut core = Core::new()?;
-        let handle = core.handle();
-        let meta_extractor = self.meta_extractor.clone();
+	pub fn run(&self) -> std::io::Result<()> {
+		let mut core = Core::new()?;
+		let handle = core.handle();
+		let meta_extractor = self.meta_extractor.clone();
 
-        let listener = TcpListener::bind(&self.listen_addr, &handle)?;
+		let listener = TcpListener::bind(&self.listen_addr, &handle)?;
 
-        let connections = listener.incoming();
-        let server = connections.for_each(move |(socket, peer_addr)| {
-            trace!(target: "tcp", "Accepted incoming connection from {}", &peer_addr);
+		let connections = listener.incoming();
+		let server = connections.for_each(move |(socket, peer_addr)| {
+			trace!(target: "tcp", "Accepted incoming connection from {}", &peer_addr);
 
-            let context = RequestContext { peer_addr: peer_addr };
-            let meta = meta_extractor.extract(&context);
+			let context = RequestContext { peer_addr: peer_addr };
+			let meta = meta_extractor.extract(&context);
 
-            let (writer, reader) = socket.framed(LineCodec).split();
-            let service = self.spawn_service(peer_addr, meta);
+			let (writer, reader) = socket.framed(LineCodec).split();
+			let service = self.spawn_service(peer_addr, meta);
 
-            let responses = reader.and_then(
-                move |req| service.call(req).then(|response|
-                    match response {
-                        Err(e) => {
-                            warn!(target: "tcp", "Error while processing request: {:?}", e);
-                            future::ok(String::new())
-                        },
-                        Ok(None) => {
-                            trace!(target: "tcp", "JSON RPC request produced no response");
-                            future::ok(String::new())
-                        },
-                        Ok(Some(response_data)) => {
-                            trace!(target: "tcp", "Sent response: {}", &response_data);
-                            future::ok(response_data)
-                        }
-                    }));
+			let responses = reader.and_then(
+				move |req| service.call(req).then(|response| match response {
+					Err(e) => {
+						warn!(target: "tcp", "Error while processing request: {:?}", e);
+						future::ok(String::new())
+					},
+					Ok(None) => {
+						trace!(target: "tcp", "JSON RPC request produced no response");
+						future::ok(String::new())
+					},
+					Ok(Some(response_data)) => {
+						trace!(target: "tcp", "Sent response: {}", &response_data);
+						future::ok(response_data)
+					}
+				})
+			);
 
-            let peer_message_queue = {
-                let (sender, receiver) = mpsc::channel(65536);
-                let mut channels = self.channels.lock().unwrap();
-                channels.insert(peer_addr.clone(), sender.clone());
+			let peer_message_queue = {
+				let (sender, receiver) = mpsc::channel(65536);
+				let mut channels = self.channels.lock().unwrap();
+				channels.insert(peer_addr.clone(), sender.clone());
 
-                PeerMessageQueue::new(
-                    responses,
-                    receiver,
-                    peer_addr.clone(),
-                )
-            };
+				PeerMessageQueue::new(
+					responses,
+					receiver,
+					peer_addr.clone(),
+				)
+			};
 
-            let shared_channels = self.channels.clone();
-            let server = writer.send_all(peer_message_queue).then(
-                move |_| {
-                    trace!(target: "tcp", "Peer {}: service finished", peer_addr);
-                    let mut channels = shared_channels.lock().unwrap();
-                    channels.remove(&peer_addr);
-                    Ok(())
-                }
-            );
-            handle.spawn(server);
+			let shared_channels = self.channels.clone();
+			let server = writer.send_all(peer_message_queue).then(move |_| {
+				trace!(target: "tcp", "Peer {}: service finished", peer_addr);
+				let mut channels = shared_channels.lock().unwrap();
+				channels.remove(&peer_addr);
+				Ok(())
+			});
+			handle.spawn(server);
 
-            Ok(())
-        });
-        core.run(server)
-    }
+			Ok(())
+		});
+		core.run(server)
+	}
 
-    pub fn dispatcher(&self) -> Dispatcher {
-        Dispatcher::new(self.channels.clone())
-    }
+	pub fn dispatcher(&self) -> Dispatcher {
+		Dispatcher::new(self.channels.clone())
+	}
 
-    fn spawn_service(&self, peer_addr: SocketAddr, meta: M) -> Service<M, S> {
-        Service::new(peer_addr.clone(), self.handler.clone(), meta)
-    }
+	fn spawn_service(&self, peer_addr: SocketAddr, meta: M) -> Service<M, S> {
+		Service::new(peer_addr.clone(), self.handler.clone(), meta)
+	}
 }
--- tcp/src/service.rs
+++ tcp/src/service.rs
@@ -7,31 +7,31 @@ use jsonrpc::{Metadata, MetaIoHandler, Middleware, NoopMiddleware};
 use jsonrpc::futures::BoxFuture;
 
 pub struct Service<M: Metadata = (), S: Middleware<M> = NoopMiddleware> {
-    handler: Arc<MetaIoHandler<M, S>>,
-    peer_addr: SocketAddr,
-    meta: M,
+	handler: Arc<MetaIoHandler<M, S>>,
+	peer_addr: SocketAddr,
+	meta: M,
 }
 
 impl<M: Metadata, S: Middleware<M>> Service<M, S> {
-    pub fn new(peer_addr: SocketAddr, handler: Arc<MetaIoHandler<M, S>>, meta: M) -> Self {
-        Service { peer_addr: peer_addr, handler: handler, meta: meta }
-    }
+	pub fn new(peer_addr: SocketAddr, handler: Arc<MetaIoHandler<M, S>>, meta: M) -> Self {
+		Service { peer_addr: peer_addr, handler: handler, meta: meta }
+	}
 }
 
 impl<M: Metadata, S: Middleware<M>> tokio_service::Service for Service<M, S> {
-    // These types must match the corresponding protocol types:
-    type Request = String;
-    type Response = Option<String>;
+	// These types must match the corresponding protocol types:
+	type Request = String;
+	type Response = Option<String>;
 
-    // For non-streaming protocols, service errors are always io::Error
-    type Error = ();
+	// For non-streaming protocols, service errors are always io::Error
+	type Error = ();
 
-    // The future for computing the response; box it for simplicity.
-    type Future = BoxFuture<Self::Response, Self::Error>;
+	// The future for computing the response; box it for simplicity.
+	type Future = BoxFuture<Self::Response, Self::Error>;
 
-    // Produce a future for computing a response from a request.
-    fn call(&self, req: Self::Request) -> Self::Future {
-        trace!(target: "tcp", "Accepted request from peer {}: {}", &self.peer_addr, req);
-        self.handler.handle_request(&req, self.meta.clone())
-    }
+	// Produce a future for computing a response from a request.
+	fn call(&self, req: Self::Request) -> Self::Future {
+		trace!(target: "tcp", "Accepted request from peer {}: {}", &self.peer_addr, req);
+		self.handler.handle_request(&req, self.meta.clone())
+	}
 }
