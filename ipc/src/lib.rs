98a2f1c5c6a13f82252407dcaa11bd2bbee14f68
NikVolf <nikvolf@gmail.com>
2017-03-14 22:57:23 +0300

    unused crates

--- ipc/src/lib.rs
+++ ipc/src/lib.rs
@@ -14,7 +14,6 @@
 // You should have received a copy of the GNU General Public License
 // along with Parity.  If not, see <http://www.gnu.org/licenses/>.
 
-extern crate rand;
 extern crate tokio_service;
 
 extern crate jsonrpc_core as jsonrpc;
