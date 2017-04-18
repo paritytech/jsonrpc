cbd19d3a9428ae093415c1a61a2f0e432272dde6
Kirill Pimenov <kirill@pimenov.cc>
2017-04-18 23:03:51 +0200

    Linux-aware multithreaded test

--- ipc/tests/multithreaded_test.rs
+++ ipc/tests/multithreaded_test.rs
@@ -8,7 +8,10 @@ mod multithreaded_test {
 	use jsonrpc_core::*;
 	use jsonrpc_ipc_server::Server;
 
+	#[cfg(windows)]
 	use miow::pipe::connect;
+	#[cfg(not(windows))]
+	use std::os::unix::net::UnixStream;
 
 	use std::thread;
 	use std::time::Duration;
@@ -18,16 +21,30 @@ mod multithreaded_test {
 	fn pipe_name() -> &'static str {
 		"\\\\.\\pipe\\Foo\\Bar\\Baz"
 	}
+	#[cfg(not(windows))]
+	fn pipe_name() -> &'static str {
+		"/tmp/foobar.sock"
+	}
 
 	#[cfg(windows)]
 	fn say_to_pipe(pipe_name: &str, message: String) -> String {
 		let mut connection = connect(pipe_name).expect("Failed to get a client connection to the pipe");
 		connection.write_all(message.as_bytes()).expect("Failed to write to the pipe");
 
-    	let mut buf = [0u8; 1024];
+		let mut buf = [0u8; 1024];
 		connection.read(&mut buf).expect("Failed to read from the pipe");
 		String::from_utf8_lossy(&buf).into_owned().trim_right_matches('\u{0}').to_string()
 	}
+	#[cfg(not(windows))]
+	fn say_to_pipe(pipe_name: &str, message: String) -> String {
+		let mut connection = UnixStream::connect(pipe_name).expect("Failed to connect to unix socket");
+		connection.write_all(message.as_bytes()).expect("Failed to write to the pipe");
+
+		let mut buf = [0u8; 1024];
+		connection.read(&mut buf).expect("Failed to read from the pipe");
+		String::from_utf8_lossy(&buf).into_owned().trim_right_matches('\u{0}').to_string()
+	}
+
 
 	fn message(n: i32) -> String {
 		format!(r#"{{ "jsonrpc":"2.0", "method":"hello", "params": {{"message": "Hello from {n}!"}}, "id": {n} }}"#, n=n)
@@ -41,12 +58,12 @@ mod multithreaded_test {
 	fn processes_several_requests_at_once() {
 		let mut io = IoHandler::new();
 		io.add_method("hello", |_params| {
-			thread::sleep(Duration::from_millis(100)); // Some ard work to block socket
+			thread::sleep(Duration::from_millis(1000)); // Some hard work to block socket
 			Ok(Value::String("hello accepted".into()))
 		});
 
 		let server = Server::new(pipe_name(), io).unwrap();
-		thread::spawn(move || server.run().unwrap());
+		thread::spawn(move || server.run());
 
 		thread::sleep(Duration::from_millis(100)); // Let's make sure the pipe server has been initialized
 
