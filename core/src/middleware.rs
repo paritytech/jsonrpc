950c64739d8dfaed4d0173483a6ff7afc0d7d1d8
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2017-07-05 14:54:57 +0200

    Process all calls in a lazy way. (#165)
    * Process all calls in a lazy way.
    
    * Get rid of .boxed().
    
    * Get rid of even more boxes.

--- core/src/middleware.rs
+++ core/src/middleware.rs
@@ -1,34 +1,44 @@
 //! `IoHandler` middlewares
 
-use io::FutureResponse;
 use calls::Metadata;
-use types::Request;
+use types::{Request, Response};
+use futures::{Future, BoxFuture};
 
 /// RPC middleware
 pub trait Middleware<M: Metadata>: Send + Sync + 'static {
+	/// A returned future.
+	type Future: Future<Item=Option<Response>, Error=()> + Send + 'static;
+
 	/// Method invoked on each request.
 	/// Allows you to either respond directly (without executing RPC call)
 	/// or do any additional work before and/or after processing the request.
-	fn on_request<F>(&self, request: Request, meta: M, next: F) -> FutureResponse where
-		F: FnOnce(Request, M) -> FutureResponse + Send;
+	fn on_request<F, X>(&self, request: Request, meta: M, next: F) -> Self::Future where
+		F: FnOnce(Request, M) -> X + Send,
+		X: Future<Item=Option<Response>, Error=()> + Send + 'static;
 }
 
 /// No-op middleware implementation
 #[derive(Default)]
 pub struct Noop;
 impl<M: Metadata> Middleware<M> for Noop {
-	fn on_request<F>(&self, request: Request, meta: M, process: F) -> FutureResponse where
-		F: FnOnce(Request, M) -> FutureResponse + Send,
+	type Future = BoxFuture<Option<Response>, ()>;
+
+	fn on_request<F, X>(&self, request: Request, meta: M, process: F) -> Self::Future where
+		F: FnOnce(Request, M) -> X + Send,
+		X: Future<Item=Option<Response>, Error=()> + Send + 'static,
 	{
-		process(request, meta)
+		process(request, meta).boxed()
 	}
 }
 
 impl<M: Metadata, A: Middleware<M>, B: Middleware<M>>
 	Middleware<M> for (A, B)
 {
-	fn on_request<F>(&self, request: Request, meta: M, process: F) -> FutureResponse where
-		F: FnOnce(Request, M) -> FutureResponse + Send,
+	type Future = A::Future;
+
+	fn on_request<F, X>(&self, request: Request, meta: M, process: F) -> Self::Future where
+		F: FnOnce(Request, M) -> X + Send,
+		X: Future<Item=Option<Response>, Error=()> + Send + 'static,
 	{
 		self.0.on_request(request, meta, move |request, meta| {
 			self.1.on_request(request, meta, process)
@@ -39,8 +49,11 @@ impl<M: Metadata, A: Middleware<M>, B: Middleware<M>>
 impl<M: Metadata, A: Middleware<M>, B: Middleware<M>, C: Middleware<M>>
 	Middleware<M> for (A, B, C)
 {
-	fn on_request<F>(&self, request: Request, meta: M, process: F) -> FutureResponse where
-		F: FnOnce(Request, M) -> FutureResponse + Send,
+	type Future = A::Future;
+
+	fn on_request<F, X>(&self, request: Request, meta: M, process: F) -> Self::Future where
+		F: FnOnce(Request, M) -> X + Send,
+		X: Future<Item=Option<Response>, Error=()> + Send + 'static,
 	{
 		self.0.on_request(request, meta, move |request, meta| {
 			self.1.on_request(request, meta, move |request, meta| {
@@ -53,8 +66,11 @@ impl<M: Metadata, A: Middleware<M>, B: Middleware<M>, C: Middleware<M>>
 impl<M: Metadata, A: Middleware<M>, B: Middleware<M>, C: Middleware<M>, D: Middleware<M>>
 	Middleware<M> for (A, B, C, D)
 {
-	fn on_request<F>(&self, request: Request, meta: M, process: F) -> FutureResponse where
-		F: FnOnce(Request, M) -> FutureResponse + Send,
+	type Future = A::Future;
+
+	fn on_request<F, X>(&self, request: Request, meta: M, process: F) -> Self::Future where
+		F: FnOnce(Request, M) -> X + Send,
+		X: Future<Item=Option<Response>, Error=()> + Send + 'static,
 	{
 		self.0.on_request(request, meta, move |request, meta| {
 			self.1.on_request(request, meta, move |request, meta| {
