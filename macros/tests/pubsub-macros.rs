b5490782884218c5ccf74cd61e54904cb3a3aeed
Marek Kotewicz <marek.kotewicz@gmail.com>
2017-08-09 14:22:32 +0200

    fixed runtime panic when there are missing trailing args (#185) (#187)
    * fixed runtime panic when there are missing trailing args for pubsub macros genereted functions
    
    * fix require_len error

---
+++ macros/tests/pubsub-macros.rs
@@ -0,0 +1,78 @@
+extern crate serde_json;
+extern crate jsonrpc_core;
+extern crate jsonrpc_pubsub;
+#[macro_use]
+extern crate jsonrpc_macros;
+
+use std::sync::Arc;
+use jsonrpc_core::futures::{future, BoxFuture, Future};
+use jsonrpc_core::futures::sync::mpsc;
+use jsonrpc_core::Error;
+use jsonrpc_pubsub::{PubSubHandler, SubscriptionId, Session, PubSubMetadata};
+use jsonrpc_macros::{pubsub, Trailing};
+
+build_rpc_trait! {
+	pub trait Rpc {
+		type Metadata;
+
+		#[pubsub(name = "hello")] {
+			/// Hello subscription
+			#[rpc(name = "hello_subscribe")]
+			fn subscribe(&self, Self::Metadata, pubsub::Subscriber<String>, u32, Trailing<u64>);
+
+			/// Unsubscribe from hello subscription.
+			#[rpc(name = "hello_unsubscribe")]
+			fn unsubscribe(&self, SubscriptionId) -> BoxFuture<bool, Error>;
+		}
+	}
+}
+
+#[derive(Default)]
+struct RpcImpl;
+
+impl Rpc for RpcImpl {
+	type Metadata = Metadata;
+
+	fn subscribe(&self, _meta: Self::Metadata, subscriber: pubsub::Subscriber<String>, _pre: u32, _trailing: Trailing<u64>) {
+		let _sink = subscriber.assign_id(SubscriptionId::Number(5));
+	}
+
+	fn unsubscribe(&self, _id: SubscriptionId) -> BoxFuture<bool, Error> {
+		future::ok(true).boxed()
+	}
+}
+
+#[derive(Clone, Default)]
+struct Metadata;
+impl jsonrpc_core::Metadata for Metadata {}
+impl PubSubMetadata for Metadata {
+	fn session(&self) -> Option<Arc<Session>> {
+		let (tx, _rx) = mpsc::channel(1);
+		Some(Arc::new(Session::new(tx)))
+	}
+}
+
+#[test]
+fn test_invalid_trailing_pubsub_params() {
+	let mut io = PubSubHandler::default();
+	let rpc = RpcImpl::default();
+	io.extend_with(rpc.to_delegate());
+
+	// when
+	let meta = Metadata;
+	let req = r#"{"jsonrpc":"2.0","id":1,"method":"hello_subscribe","params":[]}"#;
+	let res = io.handle_request_sync(req, meta);
+	let expected = r#"{
+		"jsonrpc": "2.0",
+		"error": {
+			"code": -32602,
+			"message": "Couldn't parse parameters: `params` should have at least 1 argument(s)",
+			"data": "\"\""
+		},
+		"id": 1
+	}"#;
+
+	let expected: jsonrpc_core::Response = serde_json::from_str(expected).unwrap();
+	let result: jsonrpc_core::Response = serde_json::from_str(&res.unwrap()).unwrap();
+	assert_eq!(expected, result);
+}
