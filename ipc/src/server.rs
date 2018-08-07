f4ec5cf5b781a7562696ca3f1f18da203468bfaf
Linus Färnstrand <faern@faern.net>
2018-08-07 11:36:01 +0200

    Rename session_meta_extractor to be consistent

--- ipc/src/server.rs
+++ ipc/src/server.rs
@@ -84,7 +84,7 @@ impl<M: Metadata, S: Middleware<M>> ServerBuilder<M, S> {
 	}
 
 	/// Sets session metadata extractor.
-	pub fn session_metadata_extractor<X>(mut self, meta_extractor: X) -> Self where
+	pub fn session_meta_extractor<X>(mut self, meta_extractor: X) -> Self where
 		X: MetaExtractor<M>,
 	{
 		self.meta_extractor = Arc::new(meta_extractor);
