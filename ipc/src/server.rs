b45871932f1c0728c17dd1517de6d794ec4becd1
Nicolas Gotchac <ngotchac@gmail.com>
2018-09-07 16:40:02 +0200

    Fix `parity-tokio-ipc` version

--- ipc/src/server.rs
+++ ipc/src/server.rs
@@ -1,5 +1,3 @@
-#![allow(deprecated)]
-
 use std;
 use std::sync::Arc;
 
@@ -126,17 +124,16 @@ impl<M: Metadata, S: Middleware<M>> ServerBuilder<M, S> {
 				}
 			}
 
-			let listener = Endpoint::new(endpoint_addr);
-
-			let remote = handle.remote().clone();
-			let connections = match listener.incoming(handle.clone()) {
-				Ok(connections) => connections,
+			let listener = match Endpoint::new(endpoint_addr, handle) {
+				Ok(l) => l,
 				Err(e) => {
 					start_signal.send(Err(e)).expect("Cannot fail since receiver never dropped before receiving");
 					return future::Either::A(future::ok(()));
 				}
 			};
 
+			let remote = handle.remote().clone();
+			let connections = listener.incoming();
 			let mut id = 0u64;
 
 			let server = connections.for_each(move |(io_stream, remote_id)| {
