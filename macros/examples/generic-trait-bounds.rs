c53e45fd8ef06d45583dd79e501c90b3c9021a20
Tomasz Drwięga <tomasz@parity.io>
2018-11-12 19:23:19 +0100

    Allow one-way bounds on trait generic parameters.

--- macros/examples/generic-trait-bounds.rs
+++ macros/examples/generic-trait-bounds.rs
@@ -1,12 +1,17 @@
+extern crate serde;
 extern crate jsonrpc_core;
 #[macro_use]
 extern crate jsonrpc_macros;
 
+use serde::de::DeserializeOwned;
 use jsonrpc_core::{IoHandler, Error, Result};
 use jsonrpc_core::futures::future::{self, FutureResult};
 
+// Two only requires DeserializeOwned
 build_rpc_trait! {
-	pub trait Rpc<One, Two> {
+	pub trait Rpc<One> where
+		Two: DeserializeOwned,
+	{
 		/// Get One type.
 		#[rpc(name = "getOne")]
 		fn one(&self) -> Result<One>;
@@ -18,6 +23,16 @@ build_rpc_trait! {
 		/// Performs asynchronous operation
 		#[rpc(name = "beFancy")]
 		fn call(&self, One) -> FutureResult<(One, u64), Error>;
+	}
+}
+
+build_rpc_trait! {
+	pub trait Rpc2<> where
+		Two: DeserializeOwned,
+	{
+		/// Adds two numbers and returns a result
+		#[rpc(name = "setTwo")]
+		fn set_two(&self, Two) -> Result<()>;
 	}
 }
 
@@ -38,10 +53,17 @@ impl Rpc<u64, String> for RpcImpl {
 	}
 }
 
+impl Rpc2<String> for RpcImpl {
+	fn set_two(&self, _: String) -> Result<()> {
+		unimplemented!()
+	}
+}
+
 
 fn main() {
 	let mut io = IoHandler::new();
-	let rpc = RpcImpl;
 
-	io.extend_with(rpc.to_delegate())
+	io.extend_with(Rpc::to_delegate(RpcImpl));
+	io.extend_with(Rpc2::to_delegate(RpcImpl));
 }
+
