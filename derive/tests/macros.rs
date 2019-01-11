fc77cf04925d5137eec81f643f78ac34a5956fb0
Svyatoslav Nikolsky <svyatonik@gmail.com>
2019-01-11 11:36:17 +0300

    added some tests (failing)

--- derive/tests/macros.rs
+++ derive/tests/macros.rs
@@ -21,9 +21,17 @@ pub trait Rpc {
 	#[rpc(name = "protocolVersion")]
 	fn protocol_version(&self) -> Result<String>;
 
+	/// Negates number and returns a result
+	#[rpc(name = "neg")]
+	fn neg(&self, i64) -> Result<i64>;
+
 	/// Adds two numbers and returns a result
 	#[rpc(name = "add")]
-	fn add(&self, _: u64, _: u64) -> Result<u64>;
+	fn add(&self, u64, u64) -> Result<u64>;
+
+	/// Adds up to four numbers and returns a result
+	#[rpc(name = "add_multi")]
+	fn add_multi(&self, u64, Option<u64>, Option<u64>, Option<u64>) -> Result<u64>;
 }
 
 #[derive(Default)]
@@ -34,8 +42,16 @@ impl Rpc for RpcImpl {
 		Ok("version1".into())
 	}
 
+	fn neg(&self, a: i64) -> Result<i64> {
+		Ok(-a)
+	}
+
 	fn add(&self, a: u64, b: u64) -> Result<u64> {
 		Ok(a + b)
+	}
+
+	fn add_multi(&self, a: u64, b: Option<u64>, c: Option<u64>, d: Option<u64>) -> Result<u64> {
+		Ok(a + b.unwrap_or_default() + c.unwrap_or_default() + d.unwrap_or_default())
 	}
 }
 
@@ -69,4 +85,91 @@ fn should_accept_empty_array_as_no_params() {
 
 	let result3: Response = serde_json::from_str(&res3.unwrap()).unwrap();
 	assert_eq!(expected, result3);
+}
+
+#[test]
+fn should_accept_single_param() {
+	let mut io = IoHandler::new();
+	let rpc = RpcImpl::default();
+	io.extend_with(rpc.to_delegate());
+
+	// when
+	let req1 = r#"{"jsonrpc":"2.0","id":1,"method":"neg","params":[1]}"#;
+
+	let res1 = io.handle_request_sync(req1);
+
+	// then
+	let result1: Response = serde_json::from_str(&res1.unwrap()).unwrap();
+	assert_eq!(result1, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": -1,
+		"id": 1
+	}"#).unwrap());
+}
+
+#[test]
+fn should_accept_multiple_params() {
+	let mut io = IoHandler::new();
+	let rpc = RpcImpl::default();
+	io.extend_with(rpc.to_delegate());
+
+	// when
+	let req1 = r#"{"jsonrpc":"2.0","id":1,"method":"add","params":[1, 2]}"#;
+
+	let res1 = io.handle_request_sync(req1);
+
+	// then
+	let result1: Response = serde_json::from_str(&res1.unwrap()).unwrap();
+	assert_eq!(result1, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": 3,
+		"id": 1
+	}"#).unwrap());
+}
+
+#[test]
+fn should_accept_multiple_trailing_params() {
+	let mut io = IoHandler::new();
+	let rpc = RpcImpl::default();
+	io.extend_with(rpc.to_delegate());
+
+	// when
+	let req1 = r#"{"jsonrpc":"2.0","id":1,"method":"add_multi","params":[1]}"#;
+	let req2 = r#"{"jsonrpc":"2.0","id":1,"method":"add_multi","params":[1, 2]}"#;
+	let req3 = r#"{"jsonrpc":"2.0","id":1,"method":"add_multi","params":[1, 2, 3]}"#;
+	let req4 = r#"{"jsonrpc":"2.0","id":1,"method":"add_multi","params":[1, 2, 3, 4]}"#;
+
+	let res1 = io.handle_request_sync(req1);
+	let res2 = io.handle_request_sync(req2);
+	let res3 = io.handle_request_sync(req3);
+	let res4 = io.handle_request_sync(req4);
+
+	// then
+	let result1: Response = serde_json::from_str(&res1.unwrap()).unwrap();
+	assert_eq!(result1, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": 1,
+		"id": 1
+	}"#).unwrap());
+
+	let result2: Response = serde_json::from_str(&res2.unwrap()).unwrap();
+	assert_eq!(result2, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": 3,
+		"id": 1
+	}"#).unwrap());
+
+	let result3: Response = serde_json::from_str(&res3.unwrap()).unwrap();
+	assert_eq!(result3, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": 6,
+		"id": 1
+	}"#).unwrap());
+
+	let result4: Response = serde_json::from_str(&res4.unwrap()).unwrap();
+	assert_eq!(result4, serde_json::from_str(r#"{
+		"jsonrpc": "2.0",
+		"result": 10,
+		"id": 1
+	}"#).unwrap());
 }
