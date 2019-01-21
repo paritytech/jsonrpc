be820568dd583b92158c6661a479f32ae852b008
Andrew Jones <ascjones@gmail.com>
2019-01-21 20:59:12 +0000

    Update compilefail tests for new pubsub

--- derive/tests/ui/pubsub/missing-subscribe.rs
+++ derive/tests/ui/pubsub/missing-subscribe.rs
@@ -3,16 +3,16 @@ extern crate jsonrpc_derive;
 extern crate jsonrpc_core;
 extern crate jsonrpc_pubsub;
 
-#[rpc(pubsub, name = "hello")]
+#[rpc]
 pub trait Rpc {
 	type Metadata;
 
 	/// Hello subscription
-	#[rpc(name = "hello_subscribe", alias("hello_sub"))]
+	#[pubsub(subscription = "hello", name = "hello_subscribe", alias("hello_sub"))]
 	fn subscribe(&self, Self::Metadata, typed::Subscriber<String>, u64);
 
 	/// Unsubscribe from hello subscription.
-	#[rpc(unsubscribe, name = "hello_unsubscribe")]
+	#[pubsub(subscription = "hello", unsubscribe, name = "hello_unsubscribe")]
 	fn unsubscribe(&self, Option<Self::Metadata>, SubscriptionId) -> Result<bool>;
 }
 
