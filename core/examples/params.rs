6ec3640d172c35f4ad6fd96482441a6157dda25c
Hyungsuk Kang <hskang9@gmail.com>
2019-02-16 17:54:06 +0900

    Add example for request params (#379)
    * Add example for request params
    
    * Update params.rs
    
    * Use serde_derive to import Deserialize macro

---
+++ core/examples/params.rs
@@ -0,0 +1,21 @@
+use jsonrpc_core::*;
+use serde_derive::{Deserialize};
+
+#[derive(Deserialize)]
+struct HelloParams {
+	name:String,
+}
+
+fn main() {
+	let mut io = IoHandler::new();
+
+	io.add_method("say_hello", |params: Params| {
+    let parsed: HelloParams = params.parse().unwrap();
+		Ok(Value::String(format!("hello, {}", parsed.name)))
+	});
+
+	let request = r#"{"jsonrpc": "2.0", "method": "say_hello", "params": { "name": "world" }, "id": 1}"#;
+	let response = r#"{"jsonrpc":"2.0","result":"hello, world","id":1}"#;
+
+	assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
+}
