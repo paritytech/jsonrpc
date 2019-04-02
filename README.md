c92273e93ea633fa85bd6df9fd5273634f6fd9d6
Andrew Jones <ascjones@gmail.com>
2019-04-02 10:32:08 +0100

    Add simple client examples in docs

--- README.md
+++ README.md
@@ -94,3 +94,35 @@ fn main() {
 	let mut io = jsonrpc_core::IoHandler::new();
 	io.extend_with(RpcImpl.to_delegate())
 }
+```
+
+### Client support
+
+```rust
+use jsonrpc_core::futures::future::Future;
+use jsonrpc_core::futures::sync::mpsc;
+
+#[rpc(client)]
+pub trait Rpc {
+	/// Returns a protocol version
+	#[rpc(name = "protocolVersion")]
+	fn protocol_version(&self) -> Result<String>;
+
+	/// Adds two numbers and returns a result
+	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+}
+
+fn main() {
+	let fut = {
+		let (sender, _) = mpsc::channel(0);
+		gen_client::Client::new(sender)
+			.add(5, 6)
+			.map(|res| println!("5 + 6 = {}", res))
+	};
+	fut
+		.wait()
+		.ok();
+}
+
+```
--- derive/src/lib.rs
+++ derive/src/lib.rs
@@ -123,6 +123,38 @@
 //!
 //! # fn main() {}
 //! ```
+//! 
+//! Client Example
+//! 
+//! ```
+//! use jsonrpc_derive::rpc;
+//! use jsonrpc_core::futures::future::Future;
+//! use jsonrpc_core::futures::sync::mpsc;
+//! 
+//! #[rpc(client)]
+//! pub trait Rpc {
+//! 	//! / Returns a protocol version
+//! 	#[rpc(name = "protocolVersion")]
+//! 	fn protocol_version(&self) -> Result<String>;
+//! 
+//! 	//! / Adds two numbers and returns a result
+//! 	#[rpc(name = "add", alias("callAsyncMetaAlias"))]
+//! 	fn add(&self, a: u64, b: u64) -> Result<u64>;
+//! }
+//! 
+//! fn main() {
+//! 	let fut = {
+//! 		let (sender, _) = mpsc::channel(0);
+//! 		gen_client::Client::new(sender)
+//! 			.add(5, 6)
+//! 			.map(|res| println!("5 + 6 = {}", res))
+//! 	};
+//! 	fut
+//! 		.wait()
+//! 		.ok();
+//}
+//! 
+//! ```
 
 #![recursion_limit = "256"]
 #![warn(missing_docs)]
