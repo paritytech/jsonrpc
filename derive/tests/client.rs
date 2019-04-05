1dde12a2dea1e42495633d8c864e586a3b4b0380
Andrew Jones <ascjones@gmail.com>
2019-04-05 11:13:00 +0100

    Remove derive <-> core-client circular dependency (#419)
    * Remove core-client <-> derive circular dev dependency
    
    * Restore original derived cli/server test to derive crate

---
+++ derive/tests/client.rs
@@ -0,0 +1,38 @@
+use futures::prelude::*;
+use jsonrpc_core_client::local;
+use jsonrpc_core::{IoHandler, Result};
+use jsonrpc_derive::rpc;
+
+#[rpc]
+pub trait Rpc {
+	#[rpc(name = "add")]
+	fn add(&self, a: u64, b: u64) -> Result<u64>;
+}
+
+struct RpcServer;
+
+impl Rpc for RpcServer {
+	fn add(&self, a: u64, b: u64) -> Result<u64> {
+		Ok(a + b)
+	}
+}
+
+#[test]
+fn client_server_roundtrip() {
+	let mut handler = IoHandler::new();
+	handler.extend_with(RpcServer.to_delegate());
+	let (client, rpc_client) = local::connect::<gen_client::Client, _, _>(handler);
+	let fut = client
+		.clone()
+		.add(3, 4)
+		.and_then(move |res| client.add(res, 5))
+		.join(rpc_client)
+		.map(|(res, ())| {
+			assert_eq!(res, 12);
+		})
+		.map_err(|err| {
+			eprintln!("{:?}", err);
+			assert!(false);
+		});
+	tokio::run(fut);
+}
