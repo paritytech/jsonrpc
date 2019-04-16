6b83895181251a72307668535b428147a180171e
Andrew Jones <ascjones@gmail.com>
2019-04-16 09:27:29 +0100

    Extract request/response helper function

--- core-client/src/transports/http.rs
+++ core-client/src/transports/http.rs
@@ -5,84 +5,7 @@ use hyper::rt;
 use futures::{sync::mpsc, Future, Stream, Sink};
 
 use crate::{RpcClient, RpcChannel, RpcError};
-
-struct Transport<TSink, TStream, TFuture> {
-	sink: TSink,
-	stream: TStream,
-	run: TFuture,
-}
-
-impl<TSink, TStream, TFuture> Transport<TSink, TStream, TFuture>
-where
-	TSink: Sink<SinkItem=String, SinkError=RpcError>,
-	TStream: Stream<Item=String, Error=RpcError>,
-	TFuture: Future<Item=(), Error=()>,
-{
-	pub fn with_channels<F, R>(request_buffer: usize, f: F) -> Self
-	where
-		F: Fn(String) -> R,
-		R: Future<Item=String, Error=RpcError>,
-	{
-		let (requests, requests_rx) = mpsc::channel(request_buffer);
-		let (responses, responses_rx) = mpsc::channel(0);
-
-		let run = requests_rx
-			.map(f)
-			.forward(responses
-				.sink_map_err(|_e| ())
-			)
-			.map(|_| ())
-			;
-
-		let sink = requests.sink_map_err(|e| RpcError::Other(e.into()));
-		let stream = responses_rx
-			.map_err(|()| unreachable!())
-			.and_then(|res| res);
-
-		Transport { sink, stream, run }
-	}
-
-	pub fn client<TClient: From<RpcChannel>>(&self) -> (RpcChannel, TClient) {
-		let (sender, receiver) = mpsc::channel(0);
-		(sender, RpcClient::new(&self.sink, &self.stream, receiver))
-	}
-}
-
-//fn http_client(url: &str) -> (
-//	impl Future<Item = (), Error = ()>,
-//	impl Sink<SinkItem = String, SinkError = RpcError>,
-//	impl Stream<Item = String, Error = RpcError>
-//) {
-//	let url = url.to_owned();
-//	let client = Client::new();
-//
-//	let transport = Transport::with_channels(move |request: String| {
-//		let request = Request::post(&url)
-//			.header(http::header::CONTENT_TYPE, http::header::HeaderValue::from_static("application/json"))
-//			.body(request.into())
-//			.unwrap();
-//
-//		client
-//			.request(request)
-//			.map_err(|e| RpcError::Other(e.into()))
-//			.and_then(|res| {
-//				// TODO [ToDr] Handle non-200
-//				res.into_body()
-//					.map_err(|e| RpcError::ParseError(e.to_string(), e.into()))
-//					.concat2()
-//					.map(|chunk| String::from_utf8_lossy(chunk.as_ref()).into_owned())
-//			})
-//	})
-//
-//
-//	(
-//		future,
-//		requests.sink_map_err(|e| RpcError::Other(e.into())),
-//		responses_rx
-//			.map_err(|()| unreachable!())
-//			.and_then(|res| res)
-//	)
-//}
+use super::request_response;
 
 /// Create a HTTP Client
 pub fn http<TClient>(url: &str) -> impl Future<Item=TClient, Error=()>
@@ -92,7 +15,7 @@ where
 	let url = url.to_owned();
 	let client = Client::new();
 
-	let transport = Transport::with_channels(8, move |request: String| {
+	let transport = request_response(8, move |request: String| {
 		let request = Request::post(&url)
 			.header(http::header::CONTENT_TYPE, http::header::HeaderValue::from_static("application/json"))
 			.body(request.into())
--- core-client/src/transports/mod.rs
+++ core-client/src/transports/mod.rs
@@ -1,3 +1,31 @@
 //! Client transport implementations
+use futures::{sync::mpsc, Future, Stream, Sink};
+use crate::{RpcClient, RpcChannel, RpcError};
 
 pub mod http;
+
+pub fn request_response<F, R>(request_buffer: usize, f: F) -> (
+	impl Sink<SinkItem=String, SinkError=RpcError>,
+	impl Stream<Item=String, Error=RpcError>,
+	impl Future<Item=(), Error=()>,
+) where
+	F: Fn(String) -> R,
+	R: Future<Item=String, Error=RpcError>,
+{
+	let (requests, requests_rx) = mpsc::channel(request_buffer);
+	let (responses, responses_rx) = mpsc::channel(0);
+
+	let run = requests_rx
+		.map(f)
+		.forward(responses.sink_map_err(|_e| ()))
+		.map(|_| ());
+
+	let sink = requests
+		.sink_map_err(|e| RpcError::Other(e.into()));
+
+	let stream = responses_rx
+		.map_err(|()| unreachable!())
+		.and_then(|res| res);
+
+	(sink, stream, run)
+}
