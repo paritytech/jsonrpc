73b82607a6a8c36019e3fa856c7a1d8c9f87f4e9
Andrew Jones <ascjones@gmail.com>
2019-04-17 10:52:12 +0100

    WIP: http test

--- core-client/src/transports/http.rs
+++ core-client/src/transports/http.rs
@@ -1,7 +1,6 @@
 //! HTTP client
 
 use hyper::{http, Client, Request};
-use hyper::rt;
 use futures::{Future, Stream};
 
 use crate::{RpcChannel, RpcError};
@@ -10,7 +9,7 @@ use super::request_response;
 /// Create a HTTP Client
 pub fn http<TClient>(url: &str) -> impl Future<Item=TClient, Error=()>
 where
-	TClient: Send + From<RpcChannel> + Future<Item=(), Error=RpcError>,
+	TClient: From<RpcChannel>,
 {
 	let url = url.to_owned();
 	let client = Client::new();
@@ -33,12 +32,86 @@ where
 			})
 	});
 
-	(
-		rt::lazy(move || {
-			rt::spawn(rpc_client.map_err(|e| {
-				log::error!("RPC Client error: {:?}", e);
-			}));
-			Ok(sender.into())
-		})
-	)
+	rpc_client
+		.map_err(|e| log::error!("RPC Client error: {:?}", e))
+		.map(|()| TClient::from(sender))
+}
+
+#[cfg(test)]
+mod tests {
+	use jsonrpc_core::{IoHandler, Params, Error, ErrorCode, Value};
+	use jsonrpc_http_server::*;
+	use hyper::rt;
+	use super::*;
+	use crate::*;
+
+	fn id<T>(t: T) -> T {
+		t
+	}
+
+	fn serve<F: FnOnce(ServerBuilder) -> ServerBuilder>(alter: F) -> Server {
+		let builder = ServerBuilder::new(io())
+			.cors(DomainsValidation::AllowOnly(vec![
+				AccessControlAllowOrigin::Value("parity.io".into()),
+				AccessControlAllowOrigin::Null,
+			]))
+			.cors_max_age(None)
+			.rest_api(RestApi::Secure)
+			.health_api(("/health", "hello_async"));
+
+		alter(builder).start_http(&"127.0.0.1:0".parse().unwrap()).unwrap()
+	}
+
+	fn io() -> IoHandler {
+		let mut io = IoHandler::default();
+		io.add_method("hello", |params: Params| match params.parse::<(u64,)>() {
+			Ok((num,)) => Ok(Value::String(format!("world: {}", num))),
+			_ => Ok(Value::String("world".into())),
+		});
+		io.add_method("fail", |_: Params| Err(Error::new(ErrorCode::ServerError(-34))));
+
+		io
+	}
+
+	#[derive(Clone)]
+	struct TestClient(TypedClient);
+
+	impl From<RpcChannel> for TestClient {
+		fn from(channel: RpcChannel) -> Self {
+			TestClient(channel.into())
+		}
+	}
+
+	impl TestClient {
+		fn hello(&self, msg: &'static str) -> impl Future<Item=String, Error=RpcError> {
+			println!("Client hello");
+			self.0.call_method("hello", "String", (msg,))
+		}
+	}
+
+	#[test]
+	fn should_work() {
+		env_logger::init();
+		use std::thread;
+
+		// given
+		let _server = serve(id);
+
+		println!("http test");
+
+//		rt::run(rt::lazy(|| {
+//			let uri = "127.0.0.1:0";
+//			http(uri)
+//				.and_then(|client: TestClient| {
+//					client.hello("http")
+//						.then(|result| {
+//							assert_eq!(result.unwrap(), "hello http");
+//							drop(client);
+//							Ok(())
+//						})
+//				})
+//		}));
+
+		assert!(false);
+	}
 }
