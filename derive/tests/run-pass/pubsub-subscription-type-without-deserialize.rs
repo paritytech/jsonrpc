0fab54d473e6fe94af8252528d5cc3c921680253
Andrew Jones <ascjones@gmail.com>
2019-05-13 08:14:14 +0100

    Fix up existing compilefail tests

--- derive/tests/run-pass/pubsub-subscription-type-without-deserialize.rs
+++ derive/tests/run-pass/pubsub-subscription-type-without-deserialize.rs
@@ -1,12 +1,10 @@
-extern crate serde;
 #[macro_use]
-extern crate serde_derive;
+extern crate serde;
 extern crate jsonrpc_core;
 extern crate jsonrpc_core_client;
 extern crate jsonrpc_pubsub;
 #[macro_use]
 extern crate jsonrpc_derive;
-extern crate log;
 
 use std::sync::Arc;
 use jsonrpc_core::Result;
--- derive/tests/trybuild.rs
+++ derive/tests/trybuild.rs
@@ -1,6 +1,6 @@
 #[test]
 fn compile_test() {
 	let t = trybuild::TestCases::new();
-	t.compile_fail("ui/*.rs");
-	t.pass("run-pass/*.rs");
+	t.compile_fail("tests/ui/*.rs");
+	t.pass("tests/run-pass/*.rs");
 }
