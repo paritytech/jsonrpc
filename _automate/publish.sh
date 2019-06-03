b9e5ea99b41d4bba40fe803855c73636d9b682e4
Tomasz Drwięga <tomasz@parity.io>
2019-06-03 15:17:22 +0200

    Add tagging to publish script.

--- _automate/publish.sh
+++ _automate/publish.sh
@@ -2,7 +2,10 @@
 
 set -exu
 
+VERSION=$(grep "^version" ./core/Cargo.toml | sed -e 's/.*"\(.*\)"/\1/')
 ORDER=(core core-client server-utils tcp ws ws/client http ipc stdio pubsub derive test)
+
+echo "Publishing version $VERSION"
 
 for crate in ${ORDER[@]}; do
 	cd $crate
@@ -10,3 +13,6 @@ for crate in ${ORDER[@]}; do
 	cd -
 done
 
+echo "Tagging version $VERSION"
+git tag -a v$VERSION -m "Version $VERSION"
+git push --tags
