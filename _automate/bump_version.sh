7c3cb40c29b5acc26d12a478b0fe44c64dda8c8a
Andrew Jones <ascjones@gmail.com>
2019-07-04 16:30:41 +0100

    Replace depedency version when field not in first position

--- _automate/bump_version.sh
+++ _automate/bump_version.sh
@@ -10,9 +10,9 @@ ack "^version = \"" -l | \
 	grep toml | \
 	xargs sed -i "s/^version = \".*/version = \"$VERSION\"/"
 
-ack "{ version = \"$PREV_DEPS" -l | \
+ack "[{|\,] version = \"$PREV_DEPS" -l | \
 	grep toml | \
-	xargs sed -i "s/{ version = \"$PREV_DEPS/{ version = \"$NEW_DEPS/"
+	xargs sed -i "s/\([{|\,]\) version = \"$PREV_DEPS/\1 version = \"$NEW_DEPS/"
 
 ack " = \"$PREV_DEPS" -l | \
     grep md | \
