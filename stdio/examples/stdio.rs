84fcfbd2edca2c37f35e642e4d9fa79f84b7643e
Tomasz Drwięga <tomasz@parity.io>
2020-07-13 14:46:24 +0200

    Update stdio.

--- stdio/examples/stdio.rs
+++ stdio/examples/stdio.rs
@@ -3,7 +3,7 @@ use jsonrpc_stdio_server::ServerBuilder;
 
 fn main() {
 	let mut io = IoHandler::default();
-	io.add_method("say_hello", |_params| Ok(Value::String("hello".to_owned())));
+	io.add_sync_method("say_hello", |_params| Ok(Value::String("hello".to_owned())));
 
 	ServerBuilder::new(io).build();
 }
