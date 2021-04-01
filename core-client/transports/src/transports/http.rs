609d7a6cc160742d035510fa89fb424ccf077660
Tomasz Drwięga <tomusdrw@users.noreply.github.com>
2021-04-01 15:17:21 +0200

    Bump minor version. (#624)
    * Bump minor version.
    
    * Fix build warning.
    
    * Fix ui tests.
    
    * Remove windows runner since it's decomissioned.

--- core-client/transports/src/transports/http.rs
+++ core-client/transports/src/transports/http.rs
@@ -273,7 +273,7 @@ mod tests {
 
 			if let Err(RpcError::Other(err)) = res {
 				if let Some(err) = err.downcast_ref::<hyper::Error>() {
-					assert!(err.is_connect(), format!("Expected Connection Error, got {:?}", err))
+					assert!(err.is_connect(), "Expected Connection Error, got {:?}", err)
 				} else {
 					panic!("Expected a hyper::Error")
 				}
