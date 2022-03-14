96647b3315f641fe37ff6dfa436770a62c7c81c2
Marcella Hastings <marsella@users.noreply.github.com>
2022-03-14 16:11:50 -0500

    fix examples so they don't panic (#658)

--- core/examples/async.rs
+++ core/examples/async.rs
@@ -9,7 +9,7 @@ fn main() {
 		});
 
 		let request = r#"{"jsonrpc": "2.0", "method": "say_hello", "params": [42, 23], "id": 1}"#;
-		let response = r#"{"jsonrpc":"2.0","result":"hello","id":1}"#;
+		let response = r#"{"jsonrpc":"2.0","result":"Hello World!","id":1}"#;
 
 		assert_eq!(io.handle_request(request).await, Some(response.to_owned()));
 	});
--- core/examples/basic.rs
+++ core/examples/basic.rs
@@ -6,7 +6,7 @@ fn main() {
 	io.add_sync_method("say_hello", |_: Params| Ok(Value::String("Hello World!".to_owned())));
 
 	let request = r#"{"jsonrpc": "2.0", "method": "say_hello", "params": [42, 23], "id": 1}"#;
-	let response = r#"{"jsonrpc":"2.0","result":"hello","id":1}"#;
+	let response = r#"{"jsonrpc":"2.0","result":"Hello World!","id":1}"#;
 
 	assert_eq!(io.handle_request_sync(request), Some(response.to_owned()));
 }
